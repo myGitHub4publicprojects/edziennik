1df4dae6b1d894627e221c09ee260f594ee7db0d
myGitHub4publicprojects <jszczep@gmail.com>
2017-09-10 21:07:25 +0200

    add tests

---
+++ edziennik/tests/test_views.py
@@ -0,0 +1,26 @@
+from django.test import TestCase, Client
+from django.core.urlresolvers import reverse
+from django.template.loader import render_to_string
+from django.contrib.auth.models import User
+from django.contrib import auth
+
+
+from edziennik.models import Lector, Group, Parent, Student
+
+class IndexViewTests(TestCase):
+    def test_home_view_for_not_authenticated_noerror(self):
+        """
+        if setup is correct - status code 200
+        """
+        client = Client()
+        user = auth.get_user(client) # it returns User or AnonymousUser
+        response = self.client.get(reverse('edziennik:name_home'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_home_view_for_not_authenticated_correct_template(self):
+        """
+        if user is not authenticated should see log in page 'home_for_others.html'
+        """
+        client = Client()
+        response = self.client.get(reverse('edziennik:name_home'))
+        self.assertEqual(response.templates[0].name, 'edziennik/home_for_others.html')
