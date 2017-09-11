7ec5f6aba3e8e86a80427d52f2b654413ceb1d75
myGitHub4publicprojects <jszczep@gmail.com>
2017-09-11 07:35:37 +0200

    tests

--- edziennik/tests/test_views.py
+++ edziennik/tests/test_views.py
@@ -24,3 +24,186 @@ class IndexViewTests(TestCase):
         client = Client()
         response = self.client.get(reverse('edziennik:name_home'))
         self.assertEqual(response.templates[0].name, 'edziennik/home_for_others.html')
+
+
+    def test_home_view_for_lectors_noerror(self):
+        """
+        if setup is correct - status code 200
+        """
+        user_john = User.objects.create_user(username='john',
+                                 email='jlennon@beatles.com',
+                                 password='glassonion')
+        self.client.login(username='john', password='glassonion')
+        lector = Lector.objects.create(user=user_john)
+        response = self.client.get(reverse('edziennik:name_home'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_home_view_for_lector_correct_template(self):
+        """
+        if user is lector template is home_for_lector.html
+        """
+        user_john = User.objects.create_user(username='john',
+                                 email='jlennon@beatles.com',
+                                 password='glassonion')
+        self.client.login(username='john', password='glassonion')
+        lector = Lector.objects.create(user=user_john)
+        response = self.client.get(reverse('edziennik:name_home'))
+        self.assertEqual(response.templates[0].name, 'edziennik/home_for_lector.html')
+
+    def test_home_view_for_lector_no_groups(self):
+        """
+        if lector has no groups
+        """
+        user_john = User.objects.create_user(username='john',
+                                 email='jlennon@beatles.com',
+                                 password='glassonion')
+        self.client.login(username='john', password='glassonion')
+        lector = Lector.objects.create(user=user_john)
+        response = self.client.get(reverse('edziennik:name_home'))
+        self.assertQuerysetEqual(response.context['group_list'], [])
+
+    def test_home_view_for_lector_2_groups(self):
+        """
+        if lector has 2 groups
+        """
+        user_john = User.objects.create_user(username='john',
+                                 email='jlennon@beatles.com',
+                                 password='glassonion')
+        self.client.login(username='john', password='glassonion')
+        lector = Lector.objects.create(user=user_john)
+        group1 = Group.objects.create(name='group1', lector=lector)
+        group2 = Group.objects.create(name='group2', lector=lector)
+        groups = map(repr, Group.objects.all())
+        response = self.client.get(reverse('edziennik:name_home'))
+        response_groups = list(response.context['group_list'])
+        self.assertQuerysetEqual(response_groups, groups)
+  
+
+    def test_home_view_for_admin_correct_template(self):
+        """
+        if user is admin template is home_for_admin.html
+        """
+        user_admin = User.objects.create_superuser(username='admin',
+                                 email='jlennon@beatles.com',
+                                 password='glassonion')
+        self.client.login(username='admin', password='glassonion')
+        response = self.client.get(reverse('edziennik:name_home'))
+        self.assertEqual(response.templates[0].name, 'edziennik/home_for_admin.html')
+
+    def test_home_view_for_admin_no_groups(self):
+        """
+        if there are no groups
+        """
+        user_john = User.objects.create_user(username='john',
+                                 email='jlennon@beatles.com',
+                                 password='glassonion')
+        lector = Lector.objects.create(user=user_john)
+
+        user_admin = User.objects.create_superuser(username='admin',
+                                 email='jlennon@beatles.com',
+                                 password='glassonion')
+        self.client.login(username='admin', password='glassonion')
+        response = self.client.get(reverse('edziennik:name_home'))
+        self.assertQuerysetEqual(response.context['group_list'], [])
+
+    def test_home_view_for_admin_2_groups(self):
+        """
+        if there are 2 groups
+        """
+        user_john = User.objects.create_user(username='john',
+                                 email='jlennon@beatles.com',
+                                 password='glassonion')
+        lector = Lector.objects.create(user=user_john)
+        user_admin = User.objects.create_superuser(username='admin',
+                                 email='jlennon@beatles.com',
+                                 password='glassonion')
+        self.client.login(username='admin', password='glassonion')
+        group1 = Group.objects.create(name='group1', lector=lector)
+        group2 = Group.objects.create(name='group2', lector=lector)
+        groups = map(repr, Group.objects.all())
+        response = self.client.get(reverse('edziennik:name_home'))
+        response_groups = list(response.context['group_list'])
+        self.assertQuerysetEqual(response_groups, groups)
+
+    def test_home_view_for_admin_no_lectors(self):
+        """
+        if there are no lectors
+        """
+        user_admin = User.objects.create_superuser(username='admin',
+                                 email='jlennon@beatles.com',
+                                 password='glassonion')
+        self.client.login(username='admin', password='glassonion')
+        response = self.client.get(reverse('edziennik:name_home'))
+        self.assertQuerysetEqual(response.context['lectors'], [])
+
+    def test_home_view_for_admin_2_lectors(self):
+        """
+        if there are 2 lectors
+        """
+        user_john = User.objects.create_user(username='john',
+                                 email='jlennon@beatles.com',
+                                 password='glassonion')
+        lector = Lector.objects.create(user=user_john)
+        user_john2 = User.objects.create_user(username='john2',
+                                 email='jlennon@beatles2.com',
+                                 password='glassonion2')
+        lector2 = Lector.objects.create(user=user_john2)
+        user_admin = User.objects.create_superuser(username='admin',
+                                 email='jlennon@beatles.com',
+                                 password='glassonion')
+        self.client.login(username='admin', password='glassonion')
+        lectors = map(repr, Lector.objects.all())
+        response = self.client.get(reverse('edziennik:name_home'))
+        response_lectors = list(response.context['lectors'])
+        self.assertQuerysetEqual(response_lectors, lectors)
+
+
+class LectorViewTests(TestCase):
+    def test_lector_view_for_not_admin(self):
+        """
+        if setup is correct - status code 404
+        """
+        client = Client()
+        user_john = User.objects.create_user(username='john',
+                                 email='jlennon@beatles.com',
+                                 password='glassonion')
+        lector = Lector.objects.create(user=user_john)
+        response = self.client.get(reverse('edziennik:name_lektor', args=(lector.id,)))
+        self.assertEqual(response.status_code, 404)
+
+    def test_lector_view_for_admin(self):
+        """
+        if setup is correct - status code 200
+        """
+        client = Client()
+        user_john = User.objects.create_user(username='john',
+                                 email='jlennon@beatles.com',
+                                 password='glassonion')
+        lector = Lector.objects.create(user=user_john)
+        
+        user_admin = User.objects.create_superuser(username='admin',
+                                 email='jlennon@beatles.com',
+                                 password='glassonion')
+        self.client.login(username='admin', password='glassonion')
+
+        response = self.client.get(reverse('edziennik:name_lektor', args=(lector.id,)))
+        self.assertEqual(response.status_code, 200)
+
+class StudentViewTests(TestCase):
+    def test_student_view_noerror(self):
+        """
+        if student's id exists - status code 200
+        """
+        client = Client()
+        user_john = User.objects.create_user(username='john',
+                                 email='jlennon@beatles.com',
+                                 password='glassonion')
+        user_john2 = User.objects.create_user(username='john2',
+                                 email='jlennon2@beatles.com',
+                                 password='glassonion2')
+        lector = Lector.objects.create(user=user_john)
+        group = Group.objects.create(name='group1', lector=lector)
+        parent = Parent.objects.create(user=user_john2, phone_number=123456789)
+        student = Student.objects.create(name='student1', group=group, parent=parent, gender='M')
+        response = self.client.get(reverse('edziennik:name_student', args=(student.id,)))
+        self.assertEqual(response.status_code, 200)
