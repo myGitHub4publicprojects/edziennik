546b49bde1369f807d4cdee529c923e429a98e72
myGitHub4publicprojects <jszczep@gmail.com>
2017-09-15 07:30:02 +0200

    group view test

--- edziennik/tests/test_views.py
+++ edziennik/tests/test_views.py
@@ -5,6 +5,7 @@ from django.contrib.auth.models import User
 from django.contrib import auth
 from mixer.backend.django import mixer
 import pytest
+from datetime import datetime
 
 from edziennik.models import Lector, Group, Parent, Student
 pytestmark = pytest.mark.django_db
@@ -229,6 +230,49 @@ class LectorViewTests(TestCase):
         self.assertEqual(response.status_code, 200)
         response_lector_groups= list(response.context['lectors_groups'])
         self.assertEqual(len(response_lector_groups), 2)
+
+class TestGroupView(TestCase):
+    def test_group_view_for_non_staff(self):
+        """
+        non staff user should not have access - status code 404
+        """
+        client = Client()
+        group = mixer.blend('edziennik.Group')
+        response = self.client.get(reverse('edziennik:name_group', args=(group.id,)))
+        self.assertEqual(response.status_code, 404)
+
+    def test_group_view_for_staff(self):
+        """
+        should display a list of students in this group, but not others
+        """
+        client = Client()
+        group1 = mixer.blend('edziennik.Group')
+        group2 = mixer.blend('edziennik.Group')
+        student1 = mixer.blend('edziennik.Student', group=group1)
+        student2 = mixer.blend('edziennik.Student', group=group1)
+        student3 = mixer.blend('edziennik.Student', group=group2)
+
+        today = datetime.today().date()
+        grade1 = mixer.blend('edziennik.Grades', student=student1, date_of_test=today)
+        grade2 = mixer.blend('edziennik.Grades', student=student2, date_of_test=today)
+        grade3 = mixer.blend('edziennik.Grades', student=student3, date_of_test=today)
+        user_admin = User.objects.create_superuser(username='admin',
+                                 email='jlennon@beatles.com',
+                                 password='glassonion')
+
+
+        
+
+        logged_in = self.client.login(username='admin', password='glassonion')
+        response = self.client.get(reverse('edziennik:name_group', args=(group1.id,)))
+        self.assertTrue(logged_in)
+
+        self.assertEqual(response.status_code, 200)
+        response_table_content= list(response.context['table_content'])
+
+        # table_content[0] shows first row where two first items are fixed and then students
+        self.assertEqual(len(response_table_content[0]), 2+2)
+
 
 class StudentViewTests(TestCase):
     def test_student_view_noerror(self):
