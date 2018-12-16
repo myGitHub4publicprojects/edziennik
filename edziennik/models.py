269f1465c51d72f8253960095f5bcf5c3e778877
myGitHub4publicprojects <jszczep@gmail.com>
2018-12-16 22:05:42 +0100

    Change ForeignKey fields to OneToOne in Parent and Lector. Add new model

--- edziennik/models.py
+++ edziennik/models.py
@@ -4,7 +4,7 @@ from django.db import models
 from django.conf import settings
 
 class Lector(models.Model):
-    user = models.ForeignKey(settings.AUTH_USER_MODEL,
+    user = models.OneToOneField(settings.AUTH_USER_MODEL,
                              on_delete=models.CASCADE)
 
     def __str__(self):              
@@ -18,7 +18,7 @@ class Group(models.Model):
         return self.name
 
 class Parent(models.Model):
-    user = models.ForeignKey(settings.AUTH_USER_MODEL,
+    user = models.OneToOneField(settings.AUTH_USER_MODEL,
                              on_delete=models.CASCADE)
     phone_number = models.IntegerField()
 
@@ -26,7 +26,6 @@ class Parent(models.Model):
         return self.user.username
 
 class Student(models.Model):
-    # user = models.ForeignKey(settings.AUTH_USER_MODEL, blank=True, null=True)
     name = models.CharField(max_length=200)
     group = models.ForeignKey(Group, on_delete=models.CASCADE)
     parent = models.ForeignKey(Parent, on_delete=models.CASCADE)
@@ -36,7 +35,7 @@ class Student(models.Model):
         )
     gender = models.CharField(max_length=1, choices=GENDER_CHOICES)
     quizlet = models.BooleanField(default=False) # give student reward for activity on quizlet
-    # quizlet_username = models.CharField(max_length=30, blank=True)
+    quizlet_username = models.CharField(max_length=30, blank=True)
 
     def __str__(self):              
         return self.name.encode("utf-8")
@@ -47,7 +46,7 @@ class ClassDate(models.Model):
     subject = models.CharField(max_length=200, default = 'testowy temat')
     student = models.ManyToManyField(Student, related_name="student")
     has_homework = models.ManyToManyField(Student, related_name="has_homework")
-    lector = models.ForeignKey(Lector, default=1, on_delete=models.CASCADE)
+    lector = models.ForeignKey(Lector, on_delete=models.CASCADE)
     
     def __str__(self):
         return str(self.date_of_class)
@@ -72,9 +71,9 @@ class SMS(models.Model):
     message = models.TextField(max_length=1600)
     addressee = models.ForeignKey(
         settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    delivered = models.BooleanField(default=False)
-    checked_once = models.BooleanField(default=False)
-    checked_twice = models.BooleanField(default=False)
+    delivered = models.BooleanField()
+    checked_once = models.BooleanField()
+    checked_twice = models.BooleanField()
     twilio_message_sid = models.CharField(max_length=120, null=True, blank=True)
     twilio_message_status = models.CharField(max_length=120, null=True, blank=True)
     smsapipl_message_id = models.CharField(max_length=120, null=True, blank=True)
@@ -85,9 +84,19 @@ class SMS(models.Model):
     def __str__(self):
         return self.service + self.addressee.username + str(self.timestamp)
 
-# class AdminNote(models.Model):
-#     timestamp = models.DateTimeField(auto_now_add=True)
-#     published = models.DateField(auto_now=False, auto_now_add=False)
-#     body = models.TextField(max_length=1600)
-#     student = models.ManyToManyField(Student)
-#     group = models.ManyToManyField(Group)
+
+class Admin_Profile(models.Model):
+    user = models.OneToOneField(Lector, on_delete=models.CASCADE)
+    quizlet_username = models.CharField(max_length=30, blank=True)
+    quizlet_password = models.CharField(max_length=30, blank=True)
+    twilio_account_sid = models.CharField(max_length=30, blank=True)
+    twilio_auth_token = models.CharField(max_length=30, blank=True)
+    sms_when_absent = models.BooleanField()
+    sms_when_no_homework = models.BooleanField()
+    sms_message_absence = models.CharField(max_length=300, blank=True)
+    sms_message_no_homework = models.CharField(max_length=400, blank=True)
+    school_admin_email = models.BooleanField()
+    send_email_weekly_attendance_report = models.BooleanField()
+
+    def __str__(self):
+        return self.user.username
