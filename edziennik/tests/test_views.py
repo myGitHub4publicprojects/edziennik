826069cf6b60ccd4f09d5071c2d21b3d090639a5
myGitHub4publicprojects <jszczep@gmail.com>
2018-12-17 07:42:04 +0100

    exclude migrations and tests from github

--- edziennik/tests/test_views.py
+++ edziennik/tests/test_views.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 from django.test import TestCase, Client
-from django.core.urlresolvers import reverse
+from django.urls import reverse
 from django.template.loader import render_to_string
 from django.contrib.auth.models import User
 from django.contrib import auth
