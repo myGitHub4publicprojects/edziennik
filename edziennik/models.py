d3880da9667ace7276e6c0a50da7300ead352586
myGitHub4publicprojects <jszczep@gmail.com>
2019-07-24 15:24:44 +0200

    Make group.student optional

--- edziennik/models.py
+++ edziennik/models.py
@@ -56,7 +56,7 @@ class Student(models.Model):
 class Group(models.Model):
     name = models.CharField(max_length=200)
     lector = models.ForeignKey(Lector, on_delete=models.CASCADE)
-    student = models.ManyToManyField(Student, related_name="group_student")
+    student = models.ManyToManyField(Student, related_name="group_student", blank=True)
 
     def __str__(self):
         return self.name
